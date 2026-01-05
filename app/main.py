1599dc9465342a3f4cfd2c231c0dbb3313849c12
Abdullah Saleem <mabdullahsaleem810@gmail.com>
2026-01-05 17:30:48 +0500

    Updated main.py
    Step2: Total Students , Passed and Failed Students, Bar Charts for pass and fail students.

--- app/main.py
+++ app/main.py
@@ -6,3 +6,21 @@ st.title("University Risk Analytics Dashboard")
 df=pd.read_csv('data/sample.csv')
 st.subheader('Data Preview:')
 st.dataframe(df)
+
+#Summary Metrics & Pass/Fail Chart
+st.subheader('Summary Metrics')
+
+#Total Students
+total_students=df.shape[0]
+
+#Count Passed/Fail
+passed=df[df['Pass/Fail']=='Pass'].shape[0]
+failed=df[df['Pass/Fail']=='Fail'].shape[0]
+
+st.write(f"Total Students: {total_students}")
+st.write(f"Passed : {passed}")
+st.write(f"Failed: {failed}")
+
+#Bar Chart for Pass/Fail
+st.subheader("Pass/Fail Distribution")
+st.bar_chart(df['Pass/Fail'].value_counts())
