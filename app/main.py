1104e84f473c410261a8fac9032ba906846e4e78
Abdullah Saleem <mabdullahsaleem810@gmail.com>
2026-01-05 17:23:09 +0500

    Updated main.py
    Step1: Load CSV and displayed Data preview in Streamlit.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
+import pandas as pd
 import streamlit as st
 st.title("University Risk Analytics Dashboard")
-st.write("Project in Progress")
+
+#load Data
+df=pd.read_csv('data/sample.csv')
+st.subheader('Data Preview:')
+st.dataframe(df)
